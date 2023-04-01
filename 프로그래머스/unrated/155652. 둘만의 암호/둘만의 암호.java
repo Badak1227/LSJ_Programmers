a813a1e26e15ca7bfc7d7aaa41665b0405486438
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-04-01 21:16:48 +0900

    [unrated] Title: 둘만의 암호, Time: 10.26 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -6,29 +6,16 @@ class Solution {
         int skipLen = skip.length();
         String answer = "";
 
-        List<Character> alphabet = new ArrayList<>();
-        for(int i=0; i<26; i++){
-            alphabet.add((char)('a' + i));
-        }
-
-        List<Character> skipA = new ArrayList<>();
-        for(int i=0; i< skipLen; i++) {
-            skipA.add(skip.charAt(i));
-        }
-
-        for(int i=25; i >= 0; i--){
-            if(skipA.contains(alphabet.get(i))){
-                alphabet.remove(i);
+        char[] sLetter = s.toCharArray();
+        for(int i=0; i<sLetter.length; i++){
+            char tmp = sLetter[i];
+            for(int j = 0; j<index; j++){
+                tmp = tmp == 'z' ? 'a' : (char) (tmp + 1);
+                if(skip.contains(String.valueOf(tmp))){
+                    j--;
+                }
             }
-        }
-
-        HashMap<Character, Integer> alphabetMap = new HashMap<>();
-        for(int i=0; i< alphabet.size(); i++){
-            alphabetMap.put(alphabet.get(i), i);
-        }
-
-        for(int i=0; i<sLen; i++){
-            answer += alphabet.get((alphabetMap.get(s.charAt(i)) + index) % alphabet.size());
+            answer += tmp;
         }
 
         return  answer;
