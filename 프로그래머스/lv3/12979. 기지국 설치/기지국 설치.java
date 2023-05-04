f30cd3e8da0ccee2c135a03c1635f12b1233d4f0
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-05-04 23:13:28 +0900

    [level 3] Title: 기지국 설치, Time: 0.88 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
@@ -3,6 +3,7 @@ class Solution {
         int answer = 0;
         int stationIdx = 0;
         int position = 1;
+        int wide = 2*w+1;
         
         while(position <= n){        
             // 기지국이 설치 되어 있는 경우
@@ -12,7 +13,7 @@ class Solution {
             // 기지국이 설치 되어 있지 않은 경우 
             }else{            
                 answer++;
-                position += 2*w+1;
+                position += wide;
             }            
         }
         
