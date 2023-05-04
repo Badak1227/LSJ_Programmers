df29799ba4d1b43f2485a08914dbca9ab941acc8
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-05-04 23:29:50 +0900

    [level 3] Title: 기지국 설치, Time: 0.60 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
@@ -4,19 +4,35 @@ class Solution {
         int stationIdx = 0;
         int position = 1;
         int wide = 2*w+1;
-        
-        while(position <= n){        
+        int emptySize = 0;
+
+        while(true){
             // 기지국이 설치 되어 있는 경우
-            if(stationIdx < stations.length && position >= stations[stationIdx]-w){
-                position =  stations[stationIdx]+w+1;
+            if(stationIdx < stations.length){
+                emptySize = stations[stationIdx] - w - position;
+
+                if(emptySize > 0){
+                    answer += emptySize / wide;
+                    if(emptySize % wide != 0) answer++;
+                }
+
+                position = stations[stationIdx] + w + 1;
+
                 stationIdx++;
-            // 기지국이 설치 되어 있지 않은 경우 
-            }else{            
-                answer++;
-                position += wide;
-            }            
+            }
+            // 기지국이 설치 되어 있지 않은 경우
+            else{
+                emptySize = n + 1 - position;
+
+                if(emptySize > 0){
+                    answer += emptySize / wide;
+                    if(emptySize % wide != 0) answer++;
+                }
+
+                break;
+            }
         }
-        
-        return answer;       
+
+        return answer; 
     }
 }
